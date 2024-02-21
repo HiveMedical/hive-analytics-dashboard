32e4899213408ec4d28b03daf61318005bd71b81
jake11212 <81922867+jake11212@users.noreply.github.com>
2024-02-20 23:52:20 -0600

    Update DashboardPage.js
    changed endpoint of dashboard page to be patient data metrics

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -85,7 +85,8 @@ class DashboardPage extends React.Component {
 
     // const userName = AuthForm.defaultProps.usernameInputProps.inputvalue
     fetch(
-      'https://cxlnioef6d.execute-api.us-west-1.amazonaws.com/521_getPatientData_stage/',
+      //'https://cxlnioef6d.execute-api.us-west-1.amazonaws.com/521_getPatientData_stage/',
+        'https://fyab3djam7.execute-api.us-west-1.amazonaws.com/521_GetPatientDataMetrics_Stage/',
       {
         method: 'POST',
         headers: {
